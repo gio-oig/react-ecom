332e85ff406ca43500b1688d18a6774bff66482e
giorgi <araxamiag@gmail.com>
2020-07-28 16:03:04 +0400

    Updates

---
+++ precache-manifest.b427755d6022e374babb37544fbd6433.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd023c6648ebf51c601f1b6a359367c",
+    "url": "/react-ecom/index.html"
+  },
+  {
+    "revision": "b09a21ffa9071b8aefae",
+    "url": "/react-ecom/static/css/main.3f4d1ba4.chunk.css"
+  },
+  {
+    "revision": "df013db964df79818894",
+    "url": "/react-ecom/static/js/2.27830b42.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/react-ecom/static/js/2.27830b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09a21ffa9071b8aefae",
+    "url": "/react-ecom/static/js/main.5f5f53e0.chunk.js"
+  },
+  {
+    "revision": "d8c4d508014cc39ff5c0",
+    "url": "/react-ecom/static/js/runtime-main.e0a0d9ac.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/react-ecom/static/media/4.3 crown.svg.ed28accb.svg"
+  }
+]);
