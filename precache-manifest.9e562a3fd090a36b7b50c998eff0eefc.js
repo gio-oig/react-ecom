20401165dbab59e441e56692662b778abe5c7511
giorgi <araxamiag@gmail.com>
2020-07-28 15:59:21 +0400

    Updates

---
+++ precache-manifest.9e562a3fd090a36b7b50c998eff0eefc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a02fd5bd533fd7d1e3a4ac7624e8f80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b09a21ffa9071b8aefae",
+    "url": "/static/css/main.3f4d1ba4.chunk.css"
+  },
+  {
+    "revision": "bcd6b2073b37694e2f40",
+    "url": "/static/js/2.14cfc667.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.14cfc667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09a21ffa9071b8aefae",
+    "url": "/static/js/main.5f5f53e0.chunk.js"
+  },
+  {
+    "revision": "f76557ba0498337464e8",
+    "url": "/static/js/runtime-main.ab4c3947.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/4.3 crown.svg.ed28accb.svg"
+  }
+]);
