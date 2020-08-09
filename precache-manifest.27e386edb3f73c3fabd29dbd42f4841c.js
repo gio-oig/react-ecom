a20fe66aaee81f6b9ff26dd5c7972b38377c4330
giorgi <araxamiag@gmail.com>
2020-08-09 13:22:33 +0400

    Updates

---
+++ precache-manifest.27e386edb3f73c3fabd29dbd42f4841c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0af6829362992d9b80f39ab11be429",
+    "url": "/react-ecom/index.html"
+  },
+  {
+    "revision": "580ceacecf859fa566c9",
+    "url": "/react-ecom/static/css/main.97daba1b.chunk.css"
+  },
+  {
+    "revision": "12ddf850db55afbb7aae",
+    "url": "/react-ecom/static/js/2.e794144d.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/react-ecom/static/js/2.e794144d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "580ceacecf859fa566c9",
+    "url": "/react-ecom/static/js/main.f7a9ea01.chunk.js"
+  },
+  {
+    "revision": "d8c4d508014cc39ff5c0",
+    "url": "/react-ecom/static/js/runtime-main.e0a0d9ac.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/react-ecom/static/media/11.2 shopping-bag.svg.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/react-ecom/static/media/crown.ed28accb.svg"
+  }
+]);
